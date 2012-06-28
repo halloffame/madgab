d5623fabec6edd645220a0cb026d92a244dba979
Ryan Hall <ryan.hall@biola.edu>
2012-06-28 22:49:43 +0200

    added ability to pass custom sets of word types

--- lib/madgab.rb
+++ lib/madgab.rb
@@ -1,13 +1,25 @@
 module Madgab
 
+  # The default string together of values
+  DEFAULT_STRING ||= ["the", :adv, :adj, :noun, :verb]
+
+  # The default value to join the words
+  DEFAULT_JOIN ||= "_"
+
   class << self
     # Generates a more random string of words like in the game Mad Gab
-    def generate
+    def generate(string_together=DEFAULT_STRING, join_with=DEFAULT_JOIN)
       unless defined? @@diction
         self.diction
       end
 
-      words = ["the", adv, adj, noun, verb].join('_')
+      words = string_together.map{|s|
+        if s.is_a? String
+          s
+        elsif s.is_a? Symbol
+          retrieve s
+        end
+      }.join(join_with)
     end
 
     # Sets the dictionaries
@@ -22,18 +34,12 @@ module Madgab
 
     private
 
-    def adj
-      @@diction[:adj].sample
+    def retrieve(sym)
+      begin
+        @@diction[sym].sample
+      rescue
+        "- :#{sym} is not a valid parameter -"
+      end
     end
-    def adv
-      @@diction[:adv].sample
-    end
-    def noun
-      @@diction[:noun].sample
-    end
-    def verb
-      @@diction[:verb].sample
-    end
-
   end
 end
